789d2c8b6652c6ff15ab0db0bcaa9ed4f07ff53d
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-04-19 02:23:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const posts = document.getElementById("posts");
+const users = document.getElementById("users");
+const photos = document.getElementById("photos");
+const todos = document.getElementById("todos");
+
+posts.addEventListener("click", () => {});
